e2e63dd18d4417576a15d1f36216a7a173d468a2
jiaziming <>
2019-06-27 18:48:40 +0800

    图片发送及前端展示

--- s20bbs/webchat/urls.py
+++ s20bbs/webchat/urls.py
@@ -21,6 +21,9 @@ urlpatterns = [
 
     re_path(r'^$', views.dashboard, name='chart_dashboard'),
     re_path(r'^msg_send/$', views.send_msg, name='send_msg'),
-    re_path(r'^new_msg/$',views.get_new_msgs,name ='get_new_msgs')
+    re_path(r'^new_msg/$',views.get_new_msgs,name ='get_new_msgs'),
+    re_path(r'^file_upload/$', views.file_upload,name='file_upload'),
+    re_path(r'^file_upload_progress/$', views.file_upload_progress, name='file_upload_progress'),
+    re_path(r'^delete_cache_key/$', views.delete_cache_key, name='delete_cache_key'),
 
 ]
